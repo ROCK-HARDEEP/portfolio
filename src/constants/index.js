db459e4ffcf34d3c4e4e34f9d95fcbdd559a0b8a
Hardeep B <127597608+ROCK-HARDEEP@users.noreply.github.com>
2025-05-18 11:07:52 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,252 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Skills',
+    href: '#skills',
+  },
+  {
+    id: 4,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 5,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'IND ChatAI - A New AI Model',
+    desc: 'IND ChatAI is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    subdesc:
+      'This model combines the flexibility of Google&apos;s AI tools with custom logic, allowing it to perform diverse tasks tailored to user needs.',
+    href: '',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'TALK With SIGN - Real-Time speech to sign translator',
+    desc: 'I developed a real-time AI model that converts spoken language into sign language. This system leverages speech recognition and natural language processing to instantly interpret audio input and translate it into corresponding sign language gestures or animations, bridging the communication gap for the hearing-impaired community.',
+    subdesc:
+      'It supports multiple languages, ensuring broader accessibility and inclusivity. The model is designed to be lightweight and responsive, making it suitable for integration into both mobile and web platforms.',
+    href: '',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Fine-Tuned a LLM model With Docker',
+    desc: 'I fine-tuned a Large Language Model (LLM) using predefined medical datasets to specialize in healthcare-related tasks such as symptom analysis, patient query response, and medical advice support. The model was containerized using Docker for easy deployment, scalability, and consistent performance across environments.',
+    subdesc:
+      'This setup ensures secure, efficient, and portable AI assistance tailored for the medical domain.',
+    href: '',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Cloned a Meta Agent Dashboard',
+    desc: 'I successfully cloned a Meta-style agent dashboard using React.js, replicating its modern UI/UX design and interactive features. The dashboard includes dynamic components, responsive layouts, and real-time data handling, making it a powerful tool for agent performance monitoring and management.',
+    subdesc:
+      'I implemented reusable components and optimized state management for smooth user interactions. The project also supports API integration for real-time updates and scalable deployment.',
+    href: '',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Imaginify - AI Photo Manipulation ',
+    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    subdesc:
+      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    href: '',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Front end development',
+    title: "I specialize in frontend development using React.js to build dynamic and responsive web applications.Skilled in core web technologies including HTML, CSS, and JavaScript for creating clean and interactive interfaces.Experienced in component-based architecture, API integration, and modern UI design practices.",
+    icon: '/assets/fe.png',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Backend Development',
+    title: "I specialize in backend development, building robust and scalable server-side applications.Skilled in using PHP, Node.js, and MySQL for database management, API creation, and server logic.Experienced in RESTful API integration, authentication systems, and deploying applications with tools like Docker.",
+    icon: '/assets/be.png',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Database Management',
+    title: "I have experience in managing databases using MySQL, MongoDB, and PHPMyAdmin for efficient data storage and retrieval.",
+    icon: '/assets/db.png',
+    animation: 'salute',
+  },
+];
